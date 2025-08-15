929e468e7db06b0f48784c1f2be2e9a5bd5b3c69
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2025-08-15 17:14:11 +0200

    beginning of API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import Search from "./components/Search";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3"; //define the base url
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY; //import our API key from the .env file
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+    }
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <main>
