d29c462c388967086bb388038ff4708cb2b263c2
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2025-08-22 21:45:47 +0200

    Added error handling to API call

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,24 @@ const API_BASE_URL = "https://api.themoviedb.org/3"; //define the base url
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY; //import our API key from the .env file
 
 const API_OPTIONS = {
-  method: "GET",
+  method: "GET", //define GET method
   headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${API_KEY}`,
+    accept: "application/json", //the API call will send back a JSON object
+    Authorization: `Bearer ${API_KEY}`, //needed API authorization token
   },
 };
 
+///////// MAIN APP COMPONENT ///////////
+
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState(""); //useState to manage the state of the search term in the Search bar
+  const [errorMessage, setErrorMessage] = useState(""); //to manage the errorMessage
 
   const fetchMovies = async () => {
     try {
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later!");
     }
   };
 
@@ -37,9 +41,13 @@ const App = () => {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          {/* if there is an errorMessage, then display it in a paragraph */}
+        </section>
       </div>
     </main>
   );
